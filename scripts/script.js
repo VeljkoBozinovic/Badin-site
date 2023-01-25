ae383024404d9610cf3db70f1144ec025887b346
VeljkoBozinovic <veljkobozinovic@gmail.com>
2023-01-25 16:51:40 +0100

    finalFirst

--- scripts/script.js
+++ scripts/script.js
@@ -115,14 +115,39 @@ elArrows[1].addEventListener("click", (e) => {
   e.stopPropagation();
 });
 //Job trusted
-let elJobTrusted = document.querySelectorAll(".job_trusted-images-item");
+let arrayElJobTrusted = Array.from(
+  document.querySelectorAll(".job_trusted-images-item")
+);
 let currentlyDisplayedImages = [];
+let nodeList;
 
-for (let i = 0; i < 5; i++) {
-  //currentlyDisplayedImages[i] = elJobTrusted[i];
-  currentlyDisplayedImages[i] = elJobTrusted[i];
+let el = document.querySelector(".job_trusted-images");
+
+selectFive();
+
+function selectFive() {
+  currentlyDisplayedImages = arrayElJobTrusted.splice(0, 5);
+  for (let i = 0; i < currentlyDisplayedImages.length; i++) {
+    currentlyDisplayedImages[i].classList.add("currently-displayed-images");
+  }
 }
-console.log(currentlyDisplayedImages);
+
+setInterval(() => {
+  arrayElJobTrusted.push.apply(arrayElJobTrusted, currentlyDisplayedImages);
+
+  for (let i = arrayElJobTrusted.length - 1; i > 5; i--) {
+    arrayElJobTrusted[i].classList.remove("currently-displayed-images");
+  }
+
+  el.innerHTML = null;
+  nodeList = [...arrayElJobTrusted];
+
+  for (let i = 0; i < nodeList.length; i++) {
+    el.appendChild(nodeList[i]);
+  }
+
+  selectFive();
+}, 5000);
 
 //Words
 const elMobArrows = document.querySelectorAll(".mobileArrows");
