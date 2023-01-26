bbf87392eca4a5742d02ba6eb0ff26a72cfba798
VeljkoBozinovic <veljkobozinovic@gmail.com>
2023-01-26 10:12:11 +0100

    finalSecond

--- scripts/script.js
+++ scripts/script.js
@@ -115,40 +115,41 @@ elArrows[1].addEventListener("click", (e) => {
   e.stopPropagation();
 });
 //Job trusted
-let arrayElJobTrusted = Array.from(
-  document.querySelectorAll(".job_trusted-images-item")
-);
-let currentlyDisplayedImages = [];
-let nodeList;
+if (window.innerWidth > 768) {
+  let arrayElJobTrusted = Array.from(
+    document.querySelectorAll(".job_trusted-images-item")
+  );
+  let currentlyDisplayedImages = [];
+  let nodeList;
 
-let el = document.querySelector(".job_trusted-images");
-
-selectFive();
-
-function selectFive() {
-  currentlyDisplayedImages = arrayElJobTrusted.splice(0, 5);
-  for (let i = 0; i < currentlyDisplayedImages.length; i++) {
-    currentlyDisplayedImages[i].classList.add("currently-displayed-images");
-  }
-}
-
-setInterval(() => {
-  arrayElJobTrusted.push.apply(arrayElJobTrusted, currentlyDisplayedImages);
-
-  for (let i = arrayElJobTrusted.length - 1; i > 5; i--) {
-    arrayElJobTrusted[i].classList.remove("currently-displayed-images");
-  }
-
-  el.innerHTML = null;
-  nodeList = [...arrayElJobTrusted];
-
-  for (let i = 0; i < nodeList.length; i++) {
-    el.appendChild(nodeList[i]);
-  }
+  let el = document.querySelector(".job_trusted-images");
 
   selectFive();
-}, 5000);
 
+  function selectFive() {
+    currentlyDisplayedImages = arrayElJobTrusted.splice(0, 5);
+    for (let i = 0; i < currentlyDisplayedImages.length; i++) {
+      currentlyDisplayedImages[i].classList.add("currently-displayed-images");
+    }
+  }
+
+  setInterval(() => {
+    arrayElJobTrusted.push.apply(arrayElJobTrusted, currentlyDisplayedImages);
+
+    for (let i = arrayElJobTrusted.length - 1; i > 5; i--) {
+      arrayElJobTrusted[i].classList.remove("currently-displayed-images");
+    }
+
+    el.innerHTML = null;
+    nodeList = [...arrayElJobTrusted];
+
+    for (let i = 0; i < nodeList.length; i++) {
+      el.appendChild(nodeList[i]);
+    }
+
+    selectFive();
+  }, 5000);
+}
 //Words
 const elMobArrows = document.querySelectorAll(".mobileArrows");
 const elMobImages = document.querySelectorAll(".words-tabs-images-item");
